5406a5e04d1d6d59d87c355d719ebebd65fc2d3e
Lewi1006 <coach.wind11@gmail.com>
2026-07-28 21:08:55 +0200

    feat: about static

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { LandingPage } from '../../home-components/landing-page/landing-page';
+import { About } from '../../home-components/about/about';
 @Component({
   selector: 'app-home',
-  imports: [LandingPage],
+  imports: [LandingPage, About],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
