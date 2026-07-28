d19767f0279859c2b222ee6305d314d1afd23160
Lewi1006 <coach.wind11@gmail.com>
2026-07-28 19:27:56 +0200

    feat: static landing page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { LandingPage } from '../../home-components/landing-page/landing-page';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [LandingPage],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
