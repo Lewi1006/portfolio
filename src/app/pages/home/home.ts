edde1250643ec29cdd231643f58e57abb2ffab16
Lewi1006 <coach.wind11@gmail.com>
2026-07-29 09:49:47 +0200

    feat: reference static

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,9 +3,10 @@ import { LandingPage } from '../../home-components/landing-page/landing-page';
 import { About } from '../../home-components/about/about';
 import { Skills } from '../../home-components/skills/skills';
 import { Portfolio } from '../../home-components/portfolio/portfolio';
+import { References } from '../../home-components/references/references';
 @Component({
   selector: 'app-home',
-  imports: [LandingPage, About, Skills, Portfolio],
+  imports: [LandingPage, About, Skills, Portfolio, References],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
