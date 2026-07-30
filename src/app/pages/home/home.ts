9ad29c5107d61c386e33fae21fbf234cd1edcd7f
Lewi1006 <coach.wind11@gmail.com>
2026-07-30 12:16:47 +0200

    feat: reference for loop

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -4,6 +4,7 @@ import { About } from '../../home-components/about/about';
 import { Skills } from '../../home-components/skills/skills';
 import { Portfolio } from '../../home-components/portfolio/portfolio';
 import { References } from '../../home-components/references/references';
+
 @Component({
   selector: 'app-home',
   imports: [LandingPage, About, Skills, Portfolio, References],
