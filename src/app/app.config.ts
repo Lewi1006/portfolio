1a3ea273e7dc81cbc019a4ea43636f3fc2813a23
Lewi1006 <coach.wind11@gmail.com>
2026-08-06 11:46:51 +0200

    feat: added translate service and pipe into app.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,33 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withInMemoryScrolling({
+
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
         scrollPositionRestoration: 'enabled',
-        anchorScrolling: 'enabled'
-      }))
-  ]
+        anchorScrolling: 'enabled',
+      }),
+    ),
+
+    provideHttpClient(),
+    
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: './assets/i18n/',
+        suffix: '.json',
+      }),
+      fallbackLang: 'en',
+      lang: 'en',
+    }),
+  ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
+import { TranslateService, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  standalone: true,
+  imports: [RouterOutlet, Header, Footer, TranslatePipe],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('portfolio');
+
+  private translate = inject(TranslateService);
+
+  constructor() {
+    this.translate.addLangs(['de', 'en']);
+  }
 }
