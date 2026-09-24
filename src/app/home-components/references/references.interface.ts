3e528622e19847060c16176f143891ae7aa03c2f
Lewi1006 <coach.wind11@gmail.com>
2026-09-24 16:04:17 +0200

    feat: hover blue h2 and hover ref box

--- src/app/home-components/references/references.interface.ts
+++ src/app/home-components/references/references.interface.ts
@@ -1,6 +1,6 @@
 export interface Reference{
     id: number;
-    img: string;
+    // img: string;
     nameKey: string;
     roleKey: string;
     textKey: string;
--- src/app/home-components/references/references.ts
+++ src/app/home-components/references/references.ts
@@ -11,22 +11,22 @@ import { TranslatePipe } from '@ngx-translate/core';
 export class References {
   reference: Reference[] = [
     {
-      id: 1,
-      img: 'assets/icons/quotation.svg',
+      id: 1, 
+      // img: 'assets/icons/quotation.svg',
       nameKey: 'references.references.reference1.name',
       roleKey: 'references.references.reference1.role',
       textKey: 'references.references.reference1.text',
     },
     {
       id: 2,
-      img: 'assets/icons/quotation.svg',
+      // img: 'assets/icons/quotation.svg',
       nameKey: 'references.references.reference2.name',
       roleKey: 'references.references.reference2.role',
       textKey: 'references.references.reference2.text',
     },
     {
       id: 3,
-      img: 'assets/icons/quotation.svg',
+      // img: 'assets/icons/quotation.svg',
       nameKey: 'references.references.reference3.name',
       roleKey: 'references.references.reference3.role',
       textKey: 'references.references.reference3.text',
